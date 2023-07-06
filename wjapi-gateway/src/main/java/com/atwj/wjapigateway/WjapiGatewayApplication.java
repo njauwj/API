db3085da26e2d27ce4f8e2241f475c90583511df
wj <1724031211@qq.com>
2023-07-06 14:00:26 +0800

    gateway init

---
+++ wjapi-gateway/src/main/java/com/atwj/wjapigateway/WjapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.atwj.wjapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjapiGatewayApplication.class, args);
+    }
+
+}
---
+++ wjapi-gateway/src/test/java/com/atwj/wjapigateway/WjapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwj.wjapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
